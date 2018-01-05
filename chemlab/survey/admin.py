ee459166e8bc07ffdae1ec083ee00ffe1dbbdfce
Radosław Przytuła <radoslaw.przytula@gmail.com>
2018-01-05 21:17:24 +0100

    change admin headers and titles

--- chemlab/survey/admin.py
+++ chemlab/survey/admin.py
@@ -4,6 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import *
 
+admin.site.site_title = 'ChemLab - Restricted Zone'
+admin.site.site_header = 'ChemLab - Restricted Zone'
+admin.site.index_title = 'Site administration'
+
 admin.site.register(SubstanceSurvey)
 admin.site.register(Country)
 admin.site.register(City)
