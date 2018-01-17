9a1e37c959f492010adbde0f5ad3334a6d5b9993
Radosław Przytuła <radoslaw.przytula@gmail.com>
2018-01-18 00:46:11 +0100

    add duplicate survey feature

--- chemlab/survey/admin.py
+++ chemlab/survey/admin.py
@@ -4,11 +4,23 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import *
 
+
+def duplicate_survey(modeladmin, request, queryset):
+	for o in queryset:
+		o.pk = None
+		o.save()
+
 admin.site.site_title = 'ChemLab - Restricted Zone'
 admin.site.site_header = 'ChemLab - Restricted Zone'
 admin.site.index_title = 'Site administration'
+duplicate_survey.short_description = "Duplicate selected surveys"
 
-admin.site.register(SubstanceSurvey)
+
+class SurveyAdmin(admin.ModelAdmin):
+    actions = [duplicate_survey]
+
+
+admin.site.register(SubstanceSurvey, SurveyAdmin)
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Origin)
@@ -19,3 +31,4 @@ admin.site.register(Kind)
 admin.site.register(TestMethod)
 admin.site.register(Drug)
 admin.site.register(UserCode)
+
